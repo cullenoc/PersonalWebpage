99b8f97661d700c8651abdbae3cc235a7a870cc0
cullenoc <cullenoconnell@yahoo.com>
2023-01-01 22:47:11 -0500

    add clickable folders which display in notepad to show project descriptions, update text descriptions

--- public/scripts/sidebar.js
+++ public/scripts/sidebar.js
@@ -3,7 +3,7 @@ import Typed from "typed.js";
 class Terminal {
     constructor(data, lineHeight) {
         this.sidebar = document.querySelector("#sidebar");
-        this.numRows = Math.floor(sidebar.offsetHeight / lineHeight) - 1;
+        this.numRows = Math.floor(sidebar.offsetHeight / lineHeight);
         this.numBlank = this.numRows;
         this.data = data;
         this.totalLines = data.length;
@@ -19,7 +19,7 @@ class Terminal {
         }
         let container = document.createElement("div");
         container.classList.add("assembly-container");
-        sidebar.append(container);
+        this.sidebar.append(container);
         let elem = document.createElement("span");
         elem.classList.add("assembly");
         container.append(elem);
@@ -62,3 +62,45 @@ fetchLines().then(data => {
     terminal = new Terminal(data, lineHeight);
     terminal._type();
 });
+
+let notepad = document.getElementById("notepad");
+let projectIDs = ["NYC", "Matrix", "VR", "Webpage"];
+
+const openNotepad = (id) => {
+    let currDescription = notepad.querySelector(".project-description");
+    if (currDescription !== null && currDescription.id === id) {  // correct description already rendered
+        notepad.style.display = "initial";
+        return;
+    } 
+    else if (currDescription !== null) {
+        currDescription.style.display = "none";  // hide description
+        document.body.append(currDescription);  // move description to end of document
+    }
+    let description = document.getElementById(id);
+    description.style.display = "initial";
+    notepad.querySelector("#description-box").appendChild(description);
+    notepad.style.display = "initial";
+}
+
+// Add onClick function to each folder icon to open corresponding description in a div
+let projectContainers = document.querySelectorAll(".project-container");
+for (let i = 0; i < projectContainers.length; i++) {
+    projectContainers[i].addEventListener("click", function() {
+        openNotepad(projectIDs[i])
+    });
+}
+ 
+// Add onClick to description window controls to close descriptions
+notepad.querySelector(".window-controls").addEventListener("click", function() {
+    notepad.style.display = "none";
+})
+
+
+// Set margin underneath header content based on display characteristics
+let header = document.querySelector("#header-container");
+let sidebarContainer = document.querySelector("#sidebar-container");
+if (sidebarContainer.offsetHeight > header.offsetHeight) {
+    header.style.marginBottom = "5rem";
+} else {
+    header.style.marginBottom = "1rem";
+}
